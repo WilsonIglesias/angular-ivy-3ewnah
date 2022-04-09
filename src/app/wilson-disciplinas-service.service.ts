ec0acbb6305d8d13f057fff2007762bc819d18cc
WilsonIglesias <wilsoniglesias2502@gmail.com>
2022-04-08 22:00:03 -0300

    3 files modified

--- src/app/wilson-disciplinas-service.service.ts
+++ src/app/wilson-disciplinas-service.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 
 interface Task {
-  codigo: string,
-  nome: string,
-  dia: string,
-  hora: string,
-  ementa: string
+  codigo: string;
+  nome: string;
+  dia: string;
+  hora: string;
+  ementa: string;
 }
 
 @Injectable()
@@ -16,9 +16,58 @@ export class WilsonDisciplinasServiceService {
       nome: 'Desenvolvimento para Dispositivos Móveis I',
       dia: 'Segunda',
       hora: '19:00',
-      ementa: '',
-    }]
+      ementa: 'clique',
+    },
+  
+    {
+      codigo: 'AGM004A',
+      nome: 'Negócios e Marketing e Eletrônicos',
+      dia: 'Quarta',
+      hora: '19:00',
+      ementa: 'clique',
+    },
+
+    {
+      codigo: 'IDS002A',
+      nome: 'Desenvolvimento para Servidores II',
+      dia: 'Quinta',
+      hora: '19:00',
+      ementa: 'clique',
+    },
+
+    {
+      codigo: 'ITE004A',
+      nome: 'Tópicos Especiais em Sistemas para Internet II',
+      dia: 'Sexta',
+      hora: '19:00',
+      ementa: 'clique',
+    },
+
+    {
+      codigo: 'ISW011A',
+      nome: 'Projeto de Prototipagem e Testes de Usabilidade',
+      dia: 'Sábado',
+      hora: '08:00',
+      ementa: 'clique',
+    }
+  
+  ]
 
   constructor() { }
+
+  getList(){
+    return this.list;
+
   }
+
+  add(codigo: string){
+    this.list.push();
+
+  }
+  remove(index: number){
+    this.list.splice(index, 1);
+
+  }
+}
+  
 
